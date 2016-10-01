e6657e07e9e1590de55d5737f24743feeb7649b0
Billy Conn <billyconn@leadify.net>
2016-10-01 01:07:56 -0500

    Removed some test code that made it in despite my best efforts

--- git-status.zsh
+++ git-status.zsh
@@ -1,75 +1,69 @@
-
 function git_prompt_status() {
-  ZSH_THEME_GIT_PROMPT_UNTRACKED='U'
-  ZSH_THEME_GIT_PROMPT_ADDED='A'
-  ZSH_THEME_GIT_PROMPT_MODIFIED='M'
-  ZSH_THEME_GIT_PROMPT_RENAMED='R'
-  ZSH_THEME_GIT_PROMPT_DELETED='D'
-  ZSH_THEME_GIT_PROMPT_UNMERGED='F'
-  ZSH_THEME_GIT_PROMPT_AHEAD='Z'
-  ZSH_THEME_GIT_PROMPT_DIVERGED='S'
-
-  local STATUS=''
-  local INDEX=$(command git status --porcelain -b 2>/dev/null \
-    | sed -Ee  '/^## [^ ]+ .*(behind|diverged|ahead)/!b; s/^## [^ ]+ .*(behind|diverged|ahead)/\1/' 2>/dev/null \
+  local STATUS index_lookup
+  local status_text=$({command git status --porcelain -b \
+    | sed -Ee  '/^## [^ ]+ .*(behind|diverged|ahead)/!b;
+                s/.*\[(.*)\].*/\1/g; 
+                s/[ ]//g;
+                y/,/\n/' \
     | cut -c-3 \
-    | sort -u 2>/dev/null)
-
-  local INDICES=("${(@f)${INDEX}}");
+    | sort -u } 2>/dev/null)
+  local STATUS=""
   
-  local __match_status_prefix() {
-    local MATCH=$1
-    local MATCH_LENGTH=${#MATCH}
-    for index in ${INDICES}; do
-      if [[ $index[1,$MATCH_LENGTH] == $MATCH ]]; then
-        return 0
-      fi
-    done;
-    return 1
+  # Array expansion
+  local INDICES=("${(@f)${status_text}}");
+
+  typeset -A index_lookup
+
+  # Generate a hash table of all of the index items
+  for index in ${INDICES}; do
+    index_lookup[$index]=0
+  done
+  
+  local _match_status_prefix() {
+    return $(( ${+index_lookup[$1]} == 0 ))
   }
-  if __match_status_prefix '??'; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
+
+  if _match_status_prefix '?? '; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_UNTRACKED$STATUS"
   fi
-  if __match_status_prefix 'A  '; then 
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
-  elif __match_status_prefix 'M  '; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
+  if _match_status_prefix 'A  '; then 
+    STATUS="$ZSH_TEST_GIT_PROMPT_ADDED$STATUS"
+  elif _match_status_prefix 'M  '; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_ADDED$STATUS"
   fi
 
-  if __match_status_prefix ' M '; then 
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif __match_status_prefix 'AM '; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif __match_status_prefix ' T '; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
+  if _match_status_prefix ' M '; then 
+    STATUS="$ZSH_TEST_GIT_PROMPT_MODIFIED$STATUS"
+  elif _match_status_prefix 'AM '; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_MODIFIED$STATUS"
+  elif _match_status_prefix ' T '; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_MODIFIED$STATUS"
   fi
 
-  if __match_status_prefix 'R  '; then 
-    STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
+  if _match_status_prefix 'R  '; then 
+    STATUS="$ZSH_TEST_GIT_PROMPT_RENAMED$STATUS"
   fi
-  if __match_status_prefix ' D '; then 
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  elif __match_status_prefix 'D  '; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  elif __match_status_prefix 'AD '; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
+  if _match_status_prefix ' D '; then 
+    STATUS="$ZSH_TEST_GIT_PROMPT_DELETED$STATUS"
+  elif _match_status_prefix 'D  '; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_DELETED$STATUS"
+  elif _match_status_prefix 'AD '; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_DELETED$STATUS"
   fi
-
   if $(command git rev-parse --verify refs/stash >/dev/null 2>&1); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_STASHED$STATUS"
+    STATUS="$ZSH_TEST_GIT_PROMPT_STASHED$STATUS"
   fi
-  
-  if __match_status_prefix 'UU '; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
+  if _match_status_prefix 'UU '; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_UNMERGED$STATUS"
   fi;
-  if __match_status_prefix 'ahe' ; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_AHEAD$STATUS"
+  if _match_status_prefix 'ahe' ; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_AHEAD$STATUS"
   fi
-  if __match_status_prefix 'beh' ; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_AHEAD$STATUS"
+  if _match_status_prefix 'beh' ; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_BEHIND$STATUS"
   fi
-  if __match_status_prefix 'div'; then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DIVERGED$STATUS"
+  if _match_status_prefix 'div'; then
+    STATUS="$ZSH_TEST_GIT_PROMPT_DIVERGED$STATUS"
   fi
-  echo "$STATUS"
+  echo $STATUS
 }
